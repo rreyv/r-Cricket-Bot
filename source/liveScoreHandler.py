f60a91cdccb586a49c8af47b7a51a4e2acca529a
Ravi Shrivastava <ravishr224@gmail.com>
2013-10-09 22:33:14 -0500

    created skeleton for livescore updates

--- source/liveScoreHandler.py
+++ source/liveScoreHandler.py
@@ -8,5 +8,24 @@ from emailGlobals import sendEmail
 from inboxHandler import readInbox
 
 
-def updateLiveScores:
+def updateLiveScores(r,subredditName):
+	#get list of currently running matches
+	#loop over currently running matches:
+		#matchScoreUpdater(r,subredditName,matchId)
+	pass
+
+def matchScoreUpdater(r,subredditName,matchId):
+	#from matchId, get the live thread hyperlink
+	liveThreadLink='http://www.espncricinfo.com/bangladesh-v-new-zealand-2013-14/engine/match/668949.html'
+	desktopScorecardLink=getDesktopScorecardLink(liveThreadLink)
+	liveScoreText=getLiveScoreText(desktopScorecardLink)
+	updateMatchThread(r,subredditName,matchId,liveScoreText)
+
+def getDesktopScorecardLink(liveThreadLink):
+	return liveThreadLink+'?template=desktop'
+
+def updateMatchThread(r,subredditName,matchId,liveScoreText):
+	pass
+
+def getLiveScoreText(desktopScorecardLink):
 	pass
